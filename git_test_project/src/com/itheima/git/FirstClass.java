5bb932579ca6b5980015cc120aeec3d1f970d585
shuimu123 <1056624124@qq.com>
2019-06-29 22:34:21 +0800

    把第二次修改内容工程添加到git

--- git_test_project/src/com/itheima/git/FirstClass.java
+++ git_test_project/src/com/itheima/git/FirstClass.java
@@ -4,4 +4,7 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git project");
     }
+    public void test(){
+        System.out.println("第二次修改内容");
+    }
 }
